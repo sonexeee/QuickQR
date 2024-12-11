a5ea54e02c607f3f1844f08fb800e9281283c1c3
sonexeee <nguyenhasxxx@gmail.com>
2024-12-11 15:20:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='quickqr',  # Name of the package
-    version='0.1.0',  # Initial version
-    packages=find_packages(),  # Automatically find packages in the directory
-    install_requires=[  # List of dependencies required for the package to run
-        'qrcode[pil]',  # QR Code generation library
-        'Pillow',        # Python Imaging Library (used for saving QR code images)
+    name="quickqr",  # Package name
+    version="0.2.0",  # Version number
+    packages=find_packages(),
+    install_requires=[
+        "qrcode[pil]",
+        "Pillow",
     ],
-    description='A simple Python module for generating QR codes from URLs, text, and images.',
-    long_description=open('README.md').read(),  # Read long description from the README file
-    long_description_content_type='text/markdown',  # Markdown for long description
-    author='SpaceDEV',  # Your name (or your company's name)
-    url='https://github.com/sonexeee/QuickQR/tree/main',  # URL to the project (e.g., GitHub repository)
-    classifiers=[  # Classification for PyPI to help users find the package
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # License type
-        'Operating System :: OS Independent',
+    description="A QR Code Generator with customizable options",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="TheSPACEDEV",
+    url="https://github.com/sonexeee/QuickQR",  # Your GitHub repository link
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',  # Minimum Python version required
 )
+
