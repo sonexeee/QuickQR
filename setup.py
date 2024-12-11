1f830fb7521113d105bdd48b38d7fb39b184a3ea
sonexeee <nguyenhasxxx@gmail.com>
2024-12-11 15:55:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         "qrcode[pil]",
         "Pillow",
+        "termcolor",
     ],
     description="A QR Code Generator with customizable options",
     long_description=open("README.md").read(),
